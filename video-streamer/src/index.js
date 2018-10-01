13ee4d0a3296cd24b16dcd9c762bb11652a9ba19
TinfoilxD <TinfoilxD@gmail.com>
2018-09-30 22:56:42 -0400

    initial commit

---
+++ video-streamer/src/index.js
@@ -0,0 +1,43 @@
+
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+import './style/style.css';
+
+const APP_KEY = 'AIzaSyAobBsL5n0OrQr4P_WdPrglodlZiQICu7E';
+
+class App extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			videos : [],
+			selectedVideo : null
+		};
+
+		YTSearch({key: APP_KEY, term: 'reactjs'}, (videos) => {
+			this.setState({
+				videos: videos,
+				selectedVideo: videos[0]
+			});
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<SearchBar/>
+				<VideoDetail video={this.state.selectedVideo}/>
+				<VideoList
+					onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+					videos={this.state.videos}/>
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
